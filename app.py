f86ca9bbcd369f15770102677a4f87cc1d441f28
Madson Dias <madsonddias@gmail.com>
2022-07-20 22:17:28 -0300

    The build is on fire

--- app.py
+++ app.py
@@ -1,62 +1,117 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
-import plotly.graph_objects as go
+from datetime import datetime
+from time import sleep
+
+
+def get_month(n):
+    return n-12 if n > 12 else n
 
 # Starting database
-data = pd.read_csv('dataset_streamlit.csv')
+# data = pd.read_csv('dataset_streamlit.csv')
+df_names = [f"data/pr_mais_{i}_mun.csv" for i in [1, 2, 3]]
+dfs = []
+for i, df_name in enumerate(df_names):
+    dfs.append((
+        pd
+        .read_csv(df_name)
+        .rename(columns={'Unnamed: 0': 'datetime'})
+    ))
+    dfs[i] = dfs[i].assign(
+        ano = dfs[i].datetime.apply(lambda x: x[:4]),
+        mes = dfs[i].datetime.apply(lambda x: x[5:7])
+    )
 
 #App Title
 st.title('O Guru das Chuvas')
 
 # Selecting Month, year and Region
-month_option = data['mes'].unique().tolist()
-year_option = data['ano'].unique().tolist()
-bacia_option = data['regiao_hidro'].unique().tolist()
+month_option = [
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro',
+]
 
+year_option = dfs[0]['ano'].unique().tolist()
+bacia_option = dfs[0].columns.tolist()
+bacia_option.remove('datetime')
+bacia_option.remove('mes')
+bacia_option.remove('ano')
+
+with st.expander("Mais informações"):
+    st.markdown("""
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+    when an unknown printer took a galley of type and scrambled it to make a type
+    specimen book. It has survived not only five centuries, but also the leap into
+    electronic typesetting, remaining essentially unchanged. It was popularised
+    in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+    passages, and more recently with desktop publishing software like Aldus
+    PageMaker including versions of Lorem Ipsum.
+    """)
 # Starting Select boxes
-month = st.selectbox("Selecione o mês: ", month_option, 0)
-year = st.selectbox("Selecione o ano: ", year_option, 0)
-bacia = st.selectbox("Selecione a sua Bacia Hidrográfica: ", bacia_option, 0)
+col1, col2, col3 = st.columns(3)
+# st.write(month_option, datetime.now().month-1)
+month = col1.selectbox(
+    "Mês",
+    month_option,
+    datetime.now().month-1
+)
+year = col2.selectbox(
+    "Ano",
+    year_option,
+    year_option.index(year_option[-1])
+)
+municipio = col3.selectbox(
+    "Município",
+    bacia_option
+)
 
-# Filtering df according to selections
-mask = (data['mes'] == month) & (data['ano'] == year) & (data['regiao_hidro'] == bacia )
-df = data[mask]
 
-# Preparing df to bar plot
-grupo_barras = df.groupby(['mes','regiao_hidro'])[['prev_1_mes','prev_2_meses','prev_3_meses']].mean()
-df3 = pd.DataFrame(grupo_barras).reset_index()
-df_1 = df3[['mes','regiao_hidro','prev_1_mes']]
-df_1.rename(columns = {'prev_1_mes':'Precipitação'}, inplace = True)
-df_2 = df3[['mes','regiao_hidro','prev_2_meses']]
-df_2.rename(columns = {'prev_2_meses':'Precipitação'},inplace = True)
-df_3 = df3[['mes','regiao_hidro','prev_3_meses']]
-df_3.rename(columns = {'prev_3_meses':'Precipitação'}, inplace = True)
-df3 = pd.concat([df_1,df_2, df_3], axis=0).reset_index()
-df3['mes'] = df3.index + 2
 
-df3.rename(columns={'mes':'Mês'}, inplace = True)
 # Ploting bar plot
-fig = px.bar(df3, x='Mês', y='Precipitação')
-st.plotly_chart(fig)
-
-# Explaining bar plot
-st.markdown(f"Para o mês de {month} de {year}, na Região Hidrográfica da {bacia}, teremos uma previsão média de precipitação de {round(df3['Precipitação'].values[0],2)} mm, {round(df3['Precipitação'].values[1],2)} mm e {round(df3['Precipitação'].values[2],2)} mm, para os próximos 3 meses, respectivamente.")
 
 
-# Preparing dataframe to line plot
-mask2 = (data['regiao_hidro'] == bacia ) & (data['mes'] == month)
-data2= data[mask2]
-df2 = data2.groupby(['mes','regiao_hidro', 'ano'])[['Real','prev_1_mes','prev_2_meses','prev_3_meses']].mean()
-df2 = pd.DataFrame(df2).reset_index()
-df2.rename(columns={'ano':'Ano','Real':'Precipitação histórica','prev_1_mes':'Previsão de Precipitação para 1 mês','prev_2_meses': 'Previsão de Precipitação para 2 meses', 'prev_3_meses':'Previsão de Precipitação para 3 meses'}, inplace = True)
+btn = st.button("Pergunte ao guru!")
 
-# Ploting line plot
-fig2 = px.line(df2, x= 'Ano', y=['Precipitação histórica','Previsão de Precipitação para 1 mês','Previsão de Precipitação para 2 meses','Previsão de Precipitação para 3 meses'])
-st.plotly_chart(fig2)
+if btn:
+    with st.spinner("Um momento, estou mentalizando..."):
+        sleep(3)
+        month_index = f"{month_option.index(month) + 1:02d}"
+        precipitacoes = []
+        for df in dfs:
+            precipitacoes.append(
+                df.query("mes == @month_index and ano == @year")[[municipio]].values.tolist()
+            )
+        if precipitacoes[0]:
+            precipitacoes = sum(sum(precipitacoes, []), [])
+            precipitacoes = [int(max([0, pre])) for pre in precipitacoes]
+            
+            meses = [month_option[get_month(month_option.index(month)+i+1)-1] for i in [1, 2, 3]]
+            df3 = pd.DataFrame({
+                'Precipitação (mm)': precipitacoes,
+                'Mês': meses
+            })
 
-st.markdown(f"Para o mês selecionado ({month}), podemos verificar através da linha azul a sua precipitação histórica média. As outras linhas representam as previsões de precipitação para os três próximos meses, durante os anos de 2011 a 2021.")
+            fig = px.bar(df3, x='Mês', y='Precipitação (mm)')
 
-
-
+            # Explaining bar plot
+            st.info(
+                f"""Para o mês de {month} de {year}, no município de {municipio.capitalize()},
+                teremos uma previsão média de precipitação de {precipitacoes[0]} mm, {precipitacoes[1]} mm e {precipitacoes[2]} mm,
+                para os próximos 3 meses, respectivamente."""
+            )
+            st.plotly_chart(fig)
+        else:
+            st.error("Desculpe, mas não posso realizar predições para os valores selecionados.")
 
